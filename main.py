585772080a1a3dad6811a3df9bac7a7bfba819ea
sebas <s.mogollon@utp.edu.co>
2026-02-24 19:48:50 -0500

    creacion nueva rama

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from dotenv import load_dotenv
+
+app = FastAPI()
+
+load_dotenv()
+
+@app.get("/llm{prompt}")
+async def read_root(prompt):
+#Crear una logica que me permita comunicarme con un LLM 
+    from google import genai
+
+    
+    client = genai.Client()
+
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview", contents=prompt
+    )
+    print(response.text)
+
+    return {"Respuesta": response.text}
+
